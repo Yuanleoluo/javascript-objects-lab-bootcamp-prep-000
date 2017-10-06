1e44dbb98334ccb1265c7b222bb682081768b446
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-06 03:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {prop: 1}
 
 function updateObjectWithKeyAndValue(obj, key, value){
-  var newobj = obj.assign({}, obj, key, value)
+  var newobj = obj.assign({}, obj)
   return newobj
 }
